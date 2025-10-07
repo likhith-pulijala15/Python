ba91ef154432e84533de1d7ddeb55f8e2fe4d08b
likhith-pulijala15 <likhithpulijalagit15@gmail.com>
2025-10-07 13:40:58 +0530

    Add files via upload

---
+++ Pattern.py
@@ -0,0 +1,13 @@
+#Square Pattern
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print() 
+
+#Right Angle Triangle Pattern
+n = 5
+for i in range(1,n+1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
